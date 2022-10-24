46482e833d9c87b940f31b0073d8d4e585f290bb
kiarachunga <kiarachunga28@gmail.com>
2022-10-24 12:22:58 -0300

    creacion de clases de modelo

--- codigo/consultorio/src/java/modelo/Turno.java
+++ codigo/consultorio/src/java/modelo/Turno.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author localuser
- */
+
 public class Turno {
+    private int id;
+    private boolean tratamiento;
+    private Paciente paciente;
+
+    public Turno(int id, boolean tratamiento, Paciente paciente) {
+        this.id = id;
+        this.tratamiento = tratamiento;
+        this.paciente = paciente;
+    }
+
+    public Turno(boolean tratamiento, Paciente paciente) {
+        this.tratamiento = tratamiento;
+        this.paciente = paciente;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(boolean tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        return "Turno{" + "id=" + id + ", tratamiento=" + tratamiento + ", paciente=" + paciente + '}';
+    }
+    
+    
+    
+    
     
 }
