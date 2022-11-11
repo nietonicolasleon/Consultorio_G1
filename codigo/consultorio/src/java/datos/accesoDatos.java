3945be6c20842a536d70b4a0c00b3f23c9a149c7
nietonicolasleon <nietonicolasleon@gmail.com>
2022-11-11 18:29:01 -0300

    Se agregaron funciones conectando con la BDD

--- codigo/consultorio/src/java/datos/accesoDatos.java
+++ codigo/consultorio/src/java/datos/accesoDatos.java
@@ -1,19 +1,24 @@
 package datos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.time.LocalTime;
 import modelo.*;
 
 public class accesoDatos {
+    private Connection conn;
+    private Statement comandos;
     
-    public Odontologo o1 = new Odontologo("Pedro","parques","pedroparques@gmail.com","12345");
+    private String url = "jdbc:mysql://localhost/consultorio";
+    private String user = "root";
+    private String password = "";
     
-    public Horario h1 = new Horario(o1, LocalTime.of(9, 0),LocalTime.of(12, 0),1);
-    public Horario h2 = new Horario(o1, LocalTime.of(9, 0),LocalTime.of(12, 0),2);
-    
-    public Limpieza l1 = new Limpieza("limpieza",LocalTime.of(1, 0),false);
-    
-    public Turno t1 = new Turno(l1, LocalDate.now(), LocalTime.of(0, 30), LocalTime.of(9, 0));
-    public Turno t2 = new Turno(l1, LocalDate.now(), LocalTime.of(0, 30), LocalTime.of(9, 30));
+    public Limpieza l1 = new Limpieza("Limpieza tranqui", LocalTime.of(1, 0), false);
     
     public Agenda a1 = new Agenda();
     public Agenda a2 = new Agenda();
@@ -22,9 +27,39 @@ public class accesoDatos {
     public Agenda a5 = new Agenda();
     
     public accesoDatos(){
-        a1.addHorario(h1);
-        a1.addHorario(h2);
-        a1.addTurno(t1);
-        a1.addTurno(t2);
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+            comandos = conn.createStatement();
+        } catch(ClassNotFoundException ex){
+            System.out.println("No se encontro la BDD.");
+        } catch (SQLException ex){
+            System.out.println("Usuario o password incorrectos.");
+        }
+    }
+    
+    public ArrayList<Odontologo> getOdontologos(){
+        ArrayList<Odontologo> ods = new ArrayList();
+        String sql = "select * from odontologo";
+        try{
+            ResultSet tabla = comandos.executeQuery(sql);
+            while(tabla.next()){
+                int id = tabla.getInt("idOdontologo");
+                String apellido = tabla.getString("apellido");
+                String nombre = tabla.getString("nombre");
+                String matricula = tabla.getString("matricula");
+                String mail = tabla.getString("mail");
+                Odontologo o = new Odontologo(id, nombre, apellido, mail, matricula);
+                ods.add(o);
+            }
+        }catch(SQLException ex){
+            System.out.println("ERROR:" + ex.toString());
+        }
+        return ods;
+    }
+    
+    public ArrayList<Horario> getHorarios(){
+        ArrayList<Horario> hors = new ArrayList();
+        
     }
 }
